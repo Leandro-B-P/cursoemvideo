7b05b2bc03c2fa8e23b7de4ffdfb311938628f67
Leandro <leandrobatistapereira98@gmail.com>
2017-06-13 01:15:40 -0300

    Aula 010

--- .idea/tarefamp.py
+++ .idea/tarefamp.py
@@ -1,5 +1,7 @@
 name = input('Digite seu nome: ')
+passwd = input('Digite sua senha : ')
 if name.capitalize() == 'Mary':
-    print('Hello {} acess garanted!'.format(name))
-else:
-    print('Acess Denied {}!'.format(name))
+    if passwd == 'password':
+        print('Hello {} acess garanted!'.format(name))
+    else:
+        print('Acess Denied {}!'.format(name))
